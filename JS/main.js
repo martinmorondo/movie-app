9cd7d11e56dbbcc0c12fa4f7fd7fbabfbc54b50e
Martín Morondo <martinmorondo@gmail.com>
2023-03-01 19:51:22 -0300

    I add new styles to the image

--- JS/main.js
+++ JS/main.js
@@ -22,9 +22,46 @@ function showMovies(movies) {
 
     movies.forEach((movie) => {
 
-        const { title, poster_path, vote_average, overviwew } = movie;
+        const { title, poster_path, vote_average, overview } = movie;
 
         const movieDiv = document.createElement('div');
         movieDiv.classList.add('movie');
 
-})};
+        movieDiv.innerHTML =  ` 
+            <img src = '${IMG_PATH + poster_path}' alt = '${title}'>
+            <div class = 'movie-info'>
+            <h3>${title}</h3>
+            <span class = '${getClassByRate(vote_average)}'>${vote_average}</span>
+            </div>
+            <div class = 'overview'>
+            <h3>Overview</h3>
+            ${overview}
+            </div>
+        `
+        main.appendChild(movieDiv);
+})
+};
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange';
+    } else {
+        return'red';
+    }
+};
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCH_API + searchTerm);
+        
+        search.value = '';
+    } else {
+        window.location.reload();
+    }
+});
